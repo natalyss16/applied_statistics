67e8e624613c3e019e28ce53575d51e6260ae228
Natali Tckvitishvili <natali.tckvitishvili@tum.de>
2024-08-11 14:19:41 +0200

    changes

--- worksheet.r
+++ worksheet.r
@@ -1,12 +1,10 @@
+install.packages(extraDistr)
+install.packages('ggpubr')
+install.packages('tidyverse')
+
 library(tidyverse)
 library(ggplot2)
-
-wine <- read.csv("winequality-white.csv", sep = ";")
-print(wine)
-
-wine <- mutate(wine, good = ifelse(quality > 5, 1, 0))
-
-ggplot(wine, aes(x = residual.sugar, fill = as.factor(good))) +
-geom_histogram(position = "identity", alpha = .5, bins = 100) +
-scale_fill_manual(values=c("#69b3a2", "#404080")) +
-facet_wrap(vars(good))
+library(stats)
+library(ggpubr)
+library(extraDistr)
+library(tinytex)
